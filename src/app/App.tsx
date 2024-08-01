56ce62cb0fa1d9914d7ea3b3264f3a6fd82ae3c1
belyankaa <nbelogurov.04@bk.ru>
2024-08-02 01:32:56 +0700

    done nav menu

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,24 +1,42 @@
 import React from 'react';
-import './App.css';
+import {Route, Routes, Navigate} from "react-router-dom";
+import './App.scss';
+
+import Header from "./core/layout/header/Header";
+import Footer from "./core/layout/footer/Footer";
+import Main from "./core/pages/main/Main";
+import Login from "./core/pages/authorization/login/Login";
+import Search from "./core/pages/search/Search";
+import AddPost from "./core/pages/add-post/AddPost";
+import ParticipatingEvents from "./core/pages/participating-events/ParticipatingEvents";
+import Profile from "./core/pages/profile/Profile";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Header/>
+
+            <div className="routes">
+                <Routes>
+                    {/*Авторизация*/}
+                    {/*loader={redirectIfUser} вставить в Route как интерсептор но проверить*/}
+                    <Route path="login" element={<Login/>}/>
+
+                    {/*Основаные страницы*/}
+                    <Route path="/" element={<Main/>}/>
+                    <Route path="/search" element={<Search/>}/>
+                    <Route path="/add-post" element={<AddPost/>}/>
+                    <Route path="/participating-events" element={<ParticipatingEvents/>}/>
+                    <Route path="/profile" element={<Profile/>}/>
+
+                    {/*Редирект*/}
+                    <Route path="*" element={<Navigate to="/"/>}/>
+                </Routes>
+            </div>
+
+            <Footer/>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/app/core/pages/main/Main.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Main.scss';
+
+const Main = () => {
+    return (
+        <div>
+            main
+        </div>
+    );
+};
+
+export default Main;
